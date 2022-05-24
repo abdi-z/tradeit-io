b3177ec01270817685c32af76dcb0c86e88ba4c9
Abdul Rehman Aziz <fa19-bcs-016@cuilahore.edu.pk>
2022-05-24 22:51:47 +0500

    added routes, set ejs for ssr

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 var createError = require("http-errors");
+var expressLayouts = require("express-ejs-layouts");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var homeRouter = require("./routes/home");
+var homeRouter = require("./routes");
 var usersRouter = require("./routes/users");
+var contactusRouter = require("./routes/contactus");
+var tradeRouter = require("./routes/trade");
+var createRouter = require("./routes/create");
 
 var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
+app.use(expressLayouts);
+app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
 app.use(logger("dev"));
@@ -21,6 +27,9 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", homeRouter);
 app.use("/users", usersRouter);
+app.use("/contactus", contactusRouter);
+app.use("/trade", tradeRouter);
+app.use("/create", createRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("home", { title: "Tradeit ©" });
+  res.render("index", { title: "Tradeit.gg" });
 });
 
 module.exports = router;
