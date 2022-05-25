f15d98fb9f0053008dc4f8c600ad62f2b8286738
Abdul Rehman Aziz <fa19-bcs-016@cuilahore.edu.pk>
2022-05-25 22:55:46 +0500

    added card components

--- models/trade.js
+++ models/trade.js
@@ -1,14 +1,13 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-var tradeSchema=mongoose.Schema({
-    title:String,
-    description:String,
-    location:String,
-    picture:String,
-    date:String,
-})
+var tradeSchema = mongoose.Schema({
+  title: String,
+  description: String,
+  location: String,
+  picture: String,
+  date: String,
+});
 
-
-const Trade=mongoose.model('Trade',tradeSchema);
+const Trade = mongoose.model("Trade", tradeSchema);
 
 module.exports = Trade;
