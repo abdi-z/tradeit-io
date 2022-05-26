72712d2fea8e62c4c07d9d25985a44d5e11e7b05
Abdul Rehman Aziz <fa19-bcs-016@cuilahore.edu.pk>
2022-05-26 20:51:36 +0500

    create form, trades page

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var expressLayouts = require("express-ejs-layouts");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var homeRouter = require("./routes");
 var usersRouter = require("./routes/users");
 var contactusRouter = require("./routes/contactus");
@@ -22,10 +24,11 @@ app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(bodyParser.json());
 app.use("/", homeRouter);
 app.use("/users", usersRouter);
 app.use("/contactus", contactusRouter);
--- routes/create.js
+++ routes/create.js
@@ -1,8 +1,19 @@
 var express = require("express");
 var router = express.Router();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 /* GET home page. */
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var Trade = require("../models/trade");
 router.get("/", function (req, res, next) {
   res.render("create");
 });
 
+router.post("/", jsonParser, async function (req, res, next) {
+  console.log(req.body);
+  let trade = new Trade(req.body);
+  await trade.save();
+  res.redirect("/trades");
+});
 module.exports = router;
