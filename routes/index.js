b9983099a4aeba08408b2f7e07c82fb0f53de47a
Abdul Rehman Aziz <fa19-bcs-016@cuilahore.edu.pk>
2022-05-29 17:54:53 +0500

    added delete/remove functionality for trades

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 var express = require("express");
 var router = express.Router();
+var Trade = require("../models/trade");
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Tradeit.gg" });
+});
+
+/*Delete trade*/
+router.get("/delete/:id", function (req, res, next) {
+  Trade.findByIdAndDelete(req.params.id, (err, data) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.redirect("/trades");
+    }
+  });
 });
 
 module.exports = router;
